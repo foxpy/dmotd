135efd30574b399ab0d675f3226dd7de4adb66ef
Murad <muradkarammaev@gmail.com>
2019-03-30 21:31:34 +0300

    fix uptime string format
    Do not interpret days as months.

--- src/sysinfo.c
+++ src/sysinfo.c
@@ -15,8 +15,14 @@ int uptime(char *dst, size_t len)
 
 	// dt.tm_year starts from 1970, so we just subtract it
 	dt->tm_year -= 70;
+	// dt.tm_mday is 1-31, but we need to count the quantity of days
+	dt->tm_mday -= 1;
 
-	snprintf(dst, len, "%d months, %02d:%02d:%02d",
-			dt->tm_year*12 + dt->tm_yday, dt->tm_hour, dt->tm_min, dt->tm_sec);
+	snprintf(dst, len, "%d months, %d days, %02d:%02d:%02d",
+			dt->tm_year*12 + dt->tm_mon,
+			dt->tm_mday,
+			dt->tm_hour,
+			dt->tm_min,
+			dt->tm_sec);
 	return EXIT_SUCCESS;
 }
